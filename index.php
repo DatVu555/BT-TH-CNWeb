37c42fc4fa535a80886f425026d1a4fb99096ef0
Vu Tien Dat <94444735+DatVu555@users.noreply.github.com>
2021-12-30 18:06:12 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>jQuery Ajax</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src= "js/script.js"></script>
+</head>
+<body>
+    <p>Đây là đoạn văn bản 1</p>
+    <p>Đây là đoạn văn bản 2</p>
+    <p>Đây là đoạn văn bản 3</p>
+    <p>Đây là đoạn văn bản 4</p>
+    <p>Đây là đoạn văn bản 5</p>
+    <p>Đây là đoạn văn bản 6</p>
+    <p>Đây là đoạn văn bản 7</p>
+    <p>Đây là đoạn văn bản 8</p>
+    <p>Đây là đoạn văn bản 9</p>
+    <p>Đây là đoạn văn bản 10</p>
+    
+    <button id="btnClick">Click to change</button>
+
+    
+</body>
+</html>
