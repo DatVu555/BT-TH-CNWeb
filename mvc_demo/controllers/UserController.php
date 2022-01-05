77ecc4dfa0a690adc1b596145dd1c0c4a6f10f2a
DatVu555 <94444735+DatVu555@users.noreply.github.com>
2022-01-05 12:49:51 +0700

    Up 05/01

---
+++ mvc_demo/controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+    require_once 'modelUsserModal.php';
+    class UserController{
+        function index{
+            $userModel = neww userModel();
+            require_once 'view/user/index.php';
+        }
+    }
+
+    function add(){
+
+    }
+
+    function edit(){
+        
+    }
+?>
